fd7eeec341a210373e327daf9c9437a4685b06d4
tumeraltunbas <76481188+tumeraltunbas@users.noreply.github.com>
2021-03-07 11:56:05 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Invalid product name";
+        public static string MaintenanceTime ="Maintenance time";
+        public static string ProductsListed ="Listed products";
+        public static string ProductCountOfCategoryError= "There can be a maximum of 10 products in a category";
+        public static string ProductNameAlreadyExists= "There is already another product with this name";
+        public static string CategoryLimitExceded = "New products cannot be added because the category limit has been exceeded.";
+        public static string AuthorizationDenied= "You have no authorization.";
+    }
+}
